3646595e693e41f50c912eba0b6b01ebdc5050c8
Lee Gaeul <91470334+gaeulzzang@users.noreply.github.com>
2025-05-18 22:10:32 +0900

    #3 [DESIGN] gradient style 구현

--- src/pages/myPage/MyPage.tsx
+++ src/pages/myPage/MyPage.tsx
@@ -1,7 +1,13 @@
 const MyPage = () => {
-    return (
-        <div>마이페이지</div>
-    )
-}
+  return (
+    <div className={`flex flex-col h-screen`}>
+      <h1 className={`flex p-4 justify-center`}>마이페이지</h1>
+      <div
+        className={`flex-1`}
+        style={{ backgroundImage: "var(--color-green-400)" }}
+      ></div>
+    </div>
+  );
+};
 
 export default MyPage;
