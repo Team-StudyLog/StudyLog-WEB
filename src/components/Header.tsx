b6cf5a12bd671401b5a459524a62c581357a2caf
Lee Gaeul <91470334+gaeulzzang@users.noreply.github.com>
2025-06-19 14:57:17 +0900

    #6 [DESIGN] 알람 페이지 구현

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,14 +2,14 @@ import useEasyNavigate from "../hooks/useEasyNavigate.ts";
 import { Bell, User } from "lucide-react";
 
 const Header = () => {
-  const { goHomePage, goMyPage } = useEasyNavigate();
+  const { goHomePage, goMyPage, goAlarmPage } = useEasyNavigate();
   return (
     <header className={`flex justify-between bg-gray-100 p-5`}>
       <p onClick={goHomePage} className={`text-green-300 font-logo-partial-24`}>
         StudyLog
       </p>
       <div className={`flex items-center gap-4`}>
-        <Bell size={24} className={`text-gray-600`} />
+        <Bell size={24} className={`text-gray-600`} onClick={goAlarmPage} />
         <User size={24} className={`text-gray-600`} onClick={goMyPage} />
       </div>
     </header>
---
+++ src/pages/alarm/AlarmPage.tsx
@@ -0,0 +1,10 @@
+const AlarmPage = () => {
+  return (
+    <div>
+      <h1>Alarm Page</h1>
+      <p>This is the alarm page content.</p>
+    </div>
+  );
+};
+
+export default AlarmPage;
