297967c38764032e6fde0865d2527551800a1722
Lee Gaeul <91470334+gaeulzzang@users.noreply.github.com>
2025-06-19 14:49:12 +0900

    #6 [DESIGN] 헤더 컴포넌트 구현

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+import useEasyNavigate from "../hooks/useEasyNavigate.ts";
+import { Bell, User } from "lucide-react";
+
+const Header = () => {
+  const { goHomePage, goMyPage } = useEasyNavigate();
+  return (
+    <header className={`flex justify-between bg-gray-100 p-5`}>
+      <p onClick={goHomePage} className={`text-green-300 font-logo-partial-24`}>
+        StudyLog
+      </p>
+      <div className={`flex items-center gap-4`}>
+        <Bell size={24} className={`text-gray-600`} />
+        <User size={24} className={`text-gray-600`} onClick={goMyPage} />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,16 +1,16 @@
 import useEasyNavigate from "../../hooks/useEasyNavigate.ts";
+import Header from "../../components/Header.tsx";
 
 const HomePage = () => {
   const { goMyPage, goLoginPage } = useEasyNavigate();
   return (
     <>
       <div className={`flex-col`}>
-        <header className={`flex justify-between font-head02-bold-20 p-4`}>
-          <p onClick={goMyPage} className={`text-red`}>
-            마이페이지
-          </p>
-          <p onClick={goLoginPage}>로그인</p>
-        </header>
+        <Header />
+        <p onClick={goMyPage} className={`text-red`}>
+          마이페이지
+        </p>
+        <p onClick={goLoginPage}>로그인</p>
       </div>
     </>
   );
