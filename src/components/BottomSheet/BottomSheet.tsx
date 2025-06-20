d0f7a30152eb33c11e6f1fc722efddcb805d773c
Lee Gaeul <91470334+gaeulzzang@users.noreply.github.com>
2025-06-20 15:58:26 +0900

    #6 [DESIGN] 바텀시트 구현

---
+++ src/components/BottomSheet/BottomSheet.tsx
@@ -0,0 +1,116 @@
+import { motion, type PanInfo } from "framer-motion";
+import { useState, useRef, useCallback, type ReactNode } from "react";
+import type { BottomSheetState } from "../../types/types.ts";
+import {
+  bottomSheetStyle,
+  content,
+  handler,
+  overlayStyle,
+} from "./BottomSheet.styles.ts";
+
+const OFFSET_THRESHOLD = 100;
+const DELTA_THRESHOLD = 5;
+
+const variants = {
+  opened: { top: "21vh" },
+  default: { top: "55vh" },
+  closed: { top: "100vh" },
+};
+
+const overlayVariants = {
+  opened: { display: "block" },
+  default: { display: "block" },
+  closed: { display: "none" },
+};
+
+interface BottomSheetProps {
+  animateState: BottomSheetState;
+  handleAnimateChange: (animate: BottomSheetState) => void;
+  children: ReactNode;
+}
+
+const BottomSheet = ({
+  animateState,
+  handleAnimateChange,
+  children,
+}: BottomSheetProps) => {
+  const contentRef = useRef<HTMLDivElement>(null);
+  const [isAtTop, setIsAtTop] = useState(true);
+
+  // 내부 스크롤 위치 감지 함수
+  const handleContentScroll = useCallback(() => {
+    if (contentRef.current) {
+      const newIsAtTop = contentRef.current.scrollTop === 0;
+      if (newIsAtTop !== isAtTop) {
+        setIsAtTop(newIsAtTop);
+      }
+    }
+  }, [isAtTop]);
+
+  // 드래그 방향에 따라 바텀 시트 상태 관리
+  const handleDragEnd = (_event: PointerEvent, info: PanInfo) => {
+    if (animateState === "opened" && info.offset.y < 0) return;
+
+    const { offset, delta } = info;
+    const isOverOffset = Math.abs(offset.y) > OFFSET_THRESHOLD;
+    const isOverDelta = Math.abs(delta.y) > DELTA_THRESHOLD;
+    if (!isOverOffset && !isOverDelta) return;
+
+    if (offset.y > 0) {
+      // 아래쪽으로 드래그한 경우
+      if (animateState === "opened" && isAtTop) {
+        handleAnimateChange("default");
+      } else {
+        handleAnimateChange(animateState === "opened" ? "default" : "closed");
+      }
+    } else {
+      // 위쪽으로 드래그한 경우
+      handleAnimateChange(animateState === "closed" ? "default" : "opened");
+    }
+  };
+
+  // 오버레이 클릭 시 바텀 시트 상태 변경
+  const handleOverlayTap = () => {
+    if (animateState === "opened") handleAnimateChange("default");
+    else if (animateState === "default") handleAnimateChange("closed");
+  };
+
+  return (
+    <>
+      <motion.div
+        className={bottomSheetStyle}
+        initial="default"
+        animate={animateState}
+        variants={variants}
+        transition={{ type: "spring", bounce: 0, duration: 0.5 }}
+        drag={
+          animateState !== "opened" || (animateState === "opened" && isAtTop)
+            ? "y"
+            : false
+        }
+        dragConstraints={{ top: 0, bottom: 0 }}
+        dragElastic={0}
+        onDragEnd={handleDragEnd}
+      >
+        <div className={handler} />
+        <div
+          className={content}
+          ref={contentRef}
+          onScroll={handleContentScroll}
+          style={{ overflowY: animateState === "opened" ? "auto" : "hidden" }}
+        >
+          {children}
+        </div>
+      </motion.div>
+      <motion.div
+        className={overlayStyle}
+        variants={overlayVariants}
+        initial={false}
+        animate={animateState}
+        onTap={handleOverlayTap}
+      />
+    </>
+  );
+};
+
+export default BottomSheet;
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,10 +1,16 @@
 import Header from "../../components/Header/Header.tsx";
 import { useModalActions, useModalInfo } from "../../hooks/useModal.ts";
 import Modal from "../../components/Modal/Modal.tsx";
+import { useState } from "react";
+import type { BottomSheetState } from "../../types/types.ts";
+import BottomSheet from "../../components/BottomSheet/BottomSheet.tsx";
+import BottomButton from "../../components/Button/BottomButton.tsx";
 
 const HomePage = () => {
   const { isOpen } = useModalInfo();
   const { openModal } = useModalActions();
+  const [bottomSheetState, setBottomSheetState] =
+    useState<BottomSheetState>("closed");
   return (
     <>
       <div className={`flex flex-col`}>
@@ -12,6 +18,10 @@ const HomePage = () => {
         <button onClick={openModal} className={`font-body01-bold-14`}>
           모달 열기
         </button>
+        <button onClick={() => setBottomSheetState("default")}>
+          바텀시트 열기
+        </button>
+
         {isOpen && (
           <Modal
             title={"알림"}
@@ -19,6 +29,18 @@ const HomePage = () => {
             onConfirm={() => {}}
           />
         )}
+
+        <BottomSheet
+          animateState={bottomSheetState}
+          handleAnimateChange={setBottomSheetState}
+        >
+          <div className="p-4">
+            <BottomButton
+              text={"다음"}
+              onClick={() => setBottomSheetState("closed")}
+            />
+          </div>
+        </BottomSheet>
       </div>
     </>
   );
