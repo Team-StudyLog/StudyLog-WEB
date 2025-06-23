d7d6b64ccca8dacd894212a5205264e7fe1a0c83
Lee Gaeul <91470334+gaeulzzang@users.noreply.github.com>
2025-06-23 22:42:15 +0900

    #8 [DESIGN] 홈페이지 버튼 구현

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,64 +1,29 @@
 import Header from "../../components/Header/Header.tsx";
-import { useModalActions, useModalInfo } from "../../hooks/useModal.ts";
-import Modal from "../../components/Modal/Modal.tsx";
-import { useState } from "react";
-import type { BottomSheetState } from "../../types/types.ts";
-import BottomSheet from "../../components/BottomSheet/BottomSheet.tsx";
-import BottomButton from "../../components/Button/BottomButton.tsx";
-import InputLabel from "../../components/Label/InputLabel.tsx";
-import TextInput from "../../components/Input/TextInput.tsx";
+import FriendHeader from "./components/FriendHeader.tsx";
+import { mockFriends } from "../../data/mockFriends.ts";
+import backgroundImage from "../../assets/main-background.jpg";
+import ProfileSection from "./components/ProfileSection.tsx";
+import { mockUser } from "../../data/mockUser.ts";
+import HomeButton from "./components/HomeButton.tsx";
 
 const HomePage = () => {
-  const { isOpen } = useModalInfo();
-  const { openModal } = useModalActions();
-  const [inputValue, setInputValue] = useState("");
-  const [bottomSheetState, setBottomSheetState] =
-    useState<BottomSheetState>("closed");
   return (
     <>
-      <div className={`flex flex-col gap-4`}>
+      <div className={`flex flex-col`}>
         <Header />
-        <div className={`flex flex-col mt-5 px-[26px] gap-4`}>
-          <InputLabel label={"제목을 입력하세요"} htmlFor={"title"} />
-          <TextInput
-            type={"title"}
-            value={inputValue}
-            onChange={(e) => setInputValue(e.target.value)}
-            id={"title"}
-            errorText={"20자 내외로 입력해주세요"}
-          />
-          <div className={`flex justify-center gap-4`}>
-            <button onClick={openModal} className={`font-body01-bold-14`}>
-              모달 열기
-            </button>
-            <button
-              className={`font-body01-bold-14`}
-              onClick={() => setBottomSheetState("default")}
-            >
-              바텀시트 열기
-            </button>
+        <FriendHeader friends={mockFriends} />
+        <img
+          src={backgroundImage}
+          alt="메인 배경 이미지"
+          className={`w-full h-[187px] object-cover mb-[12px]`}
+        />
+        <div className={`flex flex-col items-center px-[26px]`}>
+          <ProfileSection user={mockUser} />
+          <div className={`flex w-full justify-between gap-x-[11px]`}>
+            <HomeButton type={"archive"} onClick={() => {}} />
+            <HomeButton type={"quiz"} onClick={() => {}} />
           </div>
         </div>
-
-        {isOpen && (
-          <Modal
-            title={"알림"}
-            text={"채영님을 팔로우하시겠습니까"}
-            onConfirm={() => {}}
-          />
-        )}
-
-        <BottomSheet
-          animateState={bottomSheetState}
-          handleAnimateChange={setBottomSheetState}
-        >
-          <div className="p-4">
-            <BottomButton
-              text={"다음"}
-              onClick={() => setBottomSheetState("closed")}
-            />
-          </div>
-        </BottomSheet>
       </div>
     </>
   );
