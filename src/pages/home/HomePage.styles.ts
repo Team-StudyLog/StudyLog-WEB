abec1605076b42f0355fbec2798385523a0dcf24
Lee Gaeul <91470334+gaeulzzang@users.noreply.github.com>
2025-06-25 13:35:37 +0900

    #10 [DESIGN] 홈페이지 구현

---
+++ src/pages/home/HomePage.styles.ts
@@ -0,0 +1,11 @@
+export const homePageStyle = "flex flex-col justify-between min-h-screen";
+
+export const homeContentWrapper =
+  "flex flex-col w-full items-center flex-grow-1 mt-[40px]";
+
+export const homeUserProfile =
+  "rounded-full h-[110px] w-[110px] bg-gray-200 flex items-center justify-center";
+
+export const homeMainText = "text-gray-700 font-head05-semibold-20 mt-[16px]";
+
+export const homeSubTextWrapper = "flex font-body05-medium-14 mt-[4px]";
