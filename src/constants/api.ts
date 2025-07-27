a2ca712f3e20864d15ac62538552c650db13ea02
Lee Gaeul <91470334+gaeulzzang@users.noreply.github.com>
2025-07-27 14:31:04 +0900

    #29 [FEAT] 쿼리키, 엔드포인트 추출

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,1 +1,12 @@
 export const BASE_URL = import.meta.env.VITE_APP_BASE_URL;
+
+export const END_POINT = {
+  FETCH_MY_PAGE: "/users",
+  FETCH_USER_PROFILE: "/users/profile",
+  PATCH_USER_PROFILE: "/users/profile",
+  FETCH_FRIENDS: "/friends",
+  POST_FOLLOW: "/friends",
+  FETCH_FRIEND_CODE: "/friends",
+  FETCH_FRIEND_SEARCH: "/friends/search",
+  DELETE_UNFOLLOW: (friendId: number) => `/friends/${friendId}`,
+};
