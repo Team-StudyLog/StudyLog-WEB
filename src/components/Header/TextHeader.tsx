47d924e564176951f206dd6c52a2a52fb96ea8e1
Lee Gaeul <91470334+gaeulzzang@users.noreply.github.com>
2025-08-09 16:57:07 +0900

    #31 [REFACTOR] 텍스트 헤더 위치 조정

--- src/components/Header/TextHeader.tsx
+++ src/components/Header/TextHeader.tsx
@@ -12,11 +12,13 @@ const TextHeader = ({ text = "", onClick }: TextHeaderProps) => {
     <header
       className={`flex w-full bg-gray-100 p-5 items-center text-gray-700`}
     >
-      <ChevronLeft
-        size={28}
-        onClick={onClick ?? goBack}
-        className={`flex-shrink-0`}
-      />
+      <div className={`relative`}>
+        <ChevronLeft
+          size={28}
+          onClick={onClick ?? goBack}
+          className={`absolute left-0 top-1/2 -translate-y-1/2`}
+        />
+      </div>
       <p className={`flex-1 text-center font-head05-semibold-20`}>{text}</p>
     </header>
   );
